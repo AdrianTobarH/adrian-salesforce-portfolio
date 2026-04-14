a8156b696c77134ec62b421eed8a139082157c58
Adrian Tobar <adriantobar97@gmail.com>
2026-04-14 17:48:57 -0500

    Add functions to download CV and scroll to section

---
+++ script.js
@@ -0,0 +1,9 @@
+function downloadCV() {
+    window.open("Adrian_Tobar_CV.pdf");
+}
+
+function scrollToSection(sectionId) {
+    document.getElementById(sectionId).scrollIntoView({
+        behavior: "smooth"
+    });
+}
